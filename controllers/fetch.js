f310c83832eff226888aa056eb12b21611385195
ggeorge <gmgeorge1993@gmail.com>
2020-01-29 21:37:47 -0600

    added some major updates. Still needs debugging

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -1,28 +1,34 @@
+// Controller for our scraper
+// ============================
 var db = require("../models");
 var scrape = require("../scripts/scrape");
 
 module.exports = {
-    scrapeHeadlines: function(req, res) {
-        return scrape()
-        .then(function(articles) {
-            return db.Headline.create(articles);
-        })
-        .then(function(dbHeadline) {
-            if (dbHeadline.length === 0) {
-                res.json({
-                    message: "No new articles Today."
-                });
-            }
-            else {
-                res.json({
-                    message: "Added " + dbHeadline.length + " new articles!"
-                });
-            }
-        })
-        .catch(function(err) {
-            res.json({
-                message: "Scrape Completed!"
-            });
+  scrapeHeadlines: function(req, res) {
+    // scrape the NYT
+    return scrape()
+      .then(function(articles) {
+        // then insert articles into the db
+        return db.Headline.create(articles);
+      })
+      .then(function(dbHeadline) {
+        if (dbHeadline.length === 0) {
+          res.json({
+            message: "No new articles today. Check back tomorrow!"
+          });
+        }
+        else {
+          // Otherwise send back a count of how many new articles we got
+          res.json({
+            message: "Added " + dbHeadline.length + " new articles!"
+          });
+        }
+      })
+      .catch(function(err) {
+        // This query won't insert articles with duplicate headlines, but it will error after inserting the others
+        res.json({
+          message: "Scrape complete!!"
         });
-    }
+      });
+  }
 };
--- models/Note.js
+++ models/Note.js
@@ -1,21 +1,29 @@
-var mongoose = require("mongoose");
+// Note model
+// ==========
 
+// Require mongoose
+var mongoose = require("mongoose");
+// Create the schema class using mongoose's schema method
 var Schema = mongoose.Schema;
 
+// Create the noteSchema with the schema object
 var noteSchema = new Schema({
-    _headlineId: {
-        type: Schema.Types.ObjectId,
-        ref: "Headline"
-    },
-
-    date: {
-        type:Date,
-        default: Date.now
-    },
-
-    noteText: String
+  // The headline is the article associate with the note
+  _headlineId: {
+    type: Schema.Types.ObjectId,
+    ref: "Headline"
+  },
+  // date is just a string
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  // as is the noteText
+  noteText: String
 });
 
+// Create the Note model using the noteSchema
 var Note = mongoose.model("Note", noteSchema);
 
+// Export the Note model
 module.exports = Note;
